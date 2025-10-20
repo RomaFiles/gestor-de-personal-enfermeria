2b3ddc31f6d51025a4417a04678d26cc0e0c8022
RomaFiles <roma.mb89@gmail.com>
2025-10-20 01:38:48 -0600

    Initial Commit

---
+++ buscador/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  buscador
+//
+//  Created by Rodrigo Magaña Barocio on 20/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ buscador/buscadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  buscadorApp.swift
+//  buscador
+//
+//  Created by Rodrigo Magaña Barocio on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct buscadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
